afa0f84c5169f3ae46fcc1b108cca7ba2184d95d
mustmercan <infoman.mustafa@gmail.com>
2020-02-28 09:32:20 +0300

    init

---
+++ HB Magaza Urun Arama.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         HB Magaza Urun Arama
+// @namespace    https://github.com/mustmercan/HBUrunArama
+// @version      0.1
+// @description  HB Toplu Ürün Arama
+// @author       mustmercan
+// @match        merchant.hepsiburada.com/#ProductListing
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
